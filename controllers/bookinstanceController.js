6ec801bf59411cc30b0cbc789ae4204be7f690af
Ross Kinney <ross@rossgkinney.com>
2019-12-29 09:04:33 -0700

    Commit controller changes to fix book instance branch

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -14,32 +14,21 @@ exports.bookinstance_list = function(req, res, next) {
 };
 
 
-// Display detail page for a specific book.
-exports.book_detail = function(req, res, next) {
+// Display detail page for a specific BookInstance.
+exports.bookinstance_detail = function(req, res, next) {
 
-    async.parallel({
-        book: function(callback) {
-
-            Book.findById(req.params.id)
-              .populate('author')
-              .populate('genre')
-              .exec(callback);
-        },
-        book_instance: function(callback) {
-
-          BookInstance.find({ 'book': req.params.id })
-          .exec(callback);
-        },
-    }, function(err, results) {
-        if (err) { return next(err); }
-        if (results.book==null) { // No results.
-            var err = new Error('Book not found');
-            err.status = 404;
-            return next(err);
+    BookInstance.findById(req.params.id)
+    .populate('book')
+    .exec(function (err, bookinstance) {
+      if (err) { return next(err); }
+      if (bookinstance==null) { // No results.
+          var err = new Error('Book copy not found');
+          err.status = 404;
+          return next(err);
         }
-        // Successful, so render.
-        res.render('book_detail', { title: 'Title', book:  results.book, book_instances: results.book_instance } );
-    });
+      // Successful, so render.
+      res.render('bookinstance_detail', { title: 'Copy: '+bookinstance.book.title, bookinstance:  bookinstance});
+    })
 
 };
 
